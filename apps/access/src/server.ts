2e8910f61e5eee593305d19d3d1c15e6ff371033
Yuri Alves <yuripiresalves@gmail.com>
2023-12-13 16:37:43 -0300

    add some validations

--- apps/access/src/server.ts
+++ apps/access/src/server.ts
@@ -3,12 +3,55 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+const accesses = [
+  {
+    productId: 1,
+    productName: 'Product 1',
+    userEmail: 'yuri@gmail.com',
+    userName: 'User 1',
+    hasAccess: true,
+  },
+  {
+    productId: 2,
+    productName: 'Product 2',
+    userEmail: 'user@gmail.com',
+    userName: 'User 2',
+    hasAccess: true,
+  },
+  {
+    productId: 2,
+    productName: 'Product 2',
+    userEmail: 'a@gmail.com',
+    userName: 'User 3',
+    hasAccess: true,
+  },
+];
+
 app.post('/access', async (req, res) => {
-  const data = await req.body;
+  const { name, email, productId } = await req.body;
 
-  console.log(`[ACCESS] ${data.name} comprou o produto ${data.productId}`);
+  const access = accesses.find(
+    (access) => access.userEmail === email && access.productId === productId
+  );
 
-  return res.status(201).json({ message: 'Access allowed' });
+  if (!access) {
+    accesses.push({
+      productId,
+      productName: `Product ${productId}`,
+      userEmail: email,
+      userName: name,
+      hasAccess: true,
+    });
+
+    console.log(`[ACCESS] ${name} comprou o produto ${productId}`);
+    console.log('Tabela de acessos atualizada:');
+    console.table(accesses);
+
+    return res.status(201).json({ message: 'Access allowed' });
+  }
+
+  console.log(`[ACCESS] ${name} já possui acesso ao produto ${productId}`);
+  return res.status(409).json({ message: 'Access already allowed' });
 });
 
 app.listen(3001, () => {
--- apps/purchases/src/server.ts
+++ apps/purchases/src/server.ts
@@ -4,15 +4,44 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 100,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300,
+  },
+];
+
 app.post('/purchases', async (req, res) => {
   const data = await req.body;
 
-  console.log('[PURCHASE]', data);
-  await axios.post('http://localhost:3001/access', {
-    ...data,
-  });
+  const product = products.find((product) => product.id === data.productId);
 
-  return res.status(201).json({ message: 'Purchase created' });
+  if (!product) {
+    return res.status(400).json({ message: 'Product not found' });
+  }
+
+  try {
+    await axios.post('http://localhost:3001/access', {
+      ...data,
+    });
+
+    console.log('[PURCHASE]', data);
+    return res.status(201).json({ message: 'Purchase created' });
+  } catch (error) {
+    console.log('[PURCHASE]', error.response.data);
+    return res.status(409).json(error.response.data);
+  }
 });
 
 app.listen(3000, () => {
